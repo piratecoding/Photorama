7f839ec0d64f246c810800ae41d38b6bb75d08b0
zlatanowsky <amaratc@gmail.com>
2018-05-28 23:15:07 +0200

    Fixed authorization and authentication / added  comments section with some AJAX magic

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,10 @@
 class Post < ApplicationRecord
-
+	
+	validates :user_id, presence: true
+	belongs_to :user
 	validates :image, presence: true
-
+	has_many :comments, dependent: :destroy
 	has_attached_file :image, styles: { :medium => "640px" }
-	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/	
+	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+	validates :caption, length: { minimum: 2, maximum: 300 }	
 end
