e7c7357a2dab54cce40f60aa960cfaf0186575ac
zlatanowsky <amaratc@gmail.com>
2018-07-11 23:31:57 +0200

    Added dislike feature

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
 	before_action :authenticate_user!
-	before_action :set_post, only: [:show, :edit, :update, :destroy, :like]
+	before_action :set_post, only: [:show, :edit, :update, :destroy, :like, :unlike]
 	before_action :owned_post, only: [:edit, :update, :destroy]
 
 	def index
@@ -52,6 +52,15 @@ class PostsController < ApplicationController
         format.js
       end
 	  end
+	end
+
+	def unlike
+		if @post.unliked_by current_user
+			respond_to do |format|
+				format.html { redirect_to :back }
+				format.js
+			end
+		end
 	end
 
 	private
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,6 +1,23 @@
 module PostsHelper
-  def likers_of(post)
+  def display_likes(post)
     votes = post.votes_for.up.by_type(User)
+    return list_likers(votes) if votes.size <= 8
+    count_likers(votes)
+  end
+
+  def liked_post(post)
+    if current_user.voted_for? post
+      return link_to '', unlike_post_path(post), remote: true, id: "like_#{post.id}", 
+          class: "glyphicon glyphicon-heart"
+    else
+      link_to '', like_post_path(post), remote: true, id: "like_#{post.id}", 
+          class: "glyphicon glyphicon-heart-empty" 
+    end
+  end
+
+  private
+
+  def list_likers(votes)
     user_names = []
     unless votes.blank?
       votes.voters.each do |voter|
@@ -12,7 +29,10 @@ module PostsHelper
     end
   end
 
-  private
+  def count_likers(votes)
+    vote_count = votes.size
+    vote_count.to_s + ' likes'
+  end
 
   def like_plural(votes)
     return ' like this' if votes.count > 1
