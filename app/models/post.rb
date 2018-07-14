6d5d53c0ed94b4668ea6ffd727bf69fd6d019597
zlatanowsky <amaratc@gmail.com>
2018-07-14 23:16:59 +0200

    Added notifications

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,8 @@ class Post < ApplicationRecord
   
   belongs_to :user
   has_many :comments, dependent: :destroy
-
+  has_many :notifications, dependent: :destroy
+  
   validates :user_id, presence: true
   validates :image, presence: true
   validates :caption, length: { minimum: 3, maximum: 300 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
 
 	has_many :posts, dependent: :destroy
 	has_many :comments, dependent: :destroy
+  has_many :notifications, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
