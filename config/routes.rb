e811754ec6dfe65cf491e8f201003bf08e986cc1
zlatanowsky <amaratc@gmail.com>
2018-07-15 23:19:08 +0200

    Added following users option

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'relationships/follow_user'
+
+  get 'relationships/unfollow_user'
+
+  post ':user_name/follow_user', to: 'relationships#follow_user', as: :follow_user
+  post ':user_name/unfollow_user', to: 'relationships#unfollow_user', as: :unfollow_user
+
   get 'notifications/:id/link_through', to: 'notifications#link_through',
                                         as: :link_through
   get 'notifications', to: 'notifications#index'
